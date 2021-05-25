c4d43bd94695fe6334befc8726758fa3f2180e41
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-25 13:37:51 +0530

    Fetching from sqlite functionality added

---
+++ lib/Widgets/location_input.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+        height: 170, width: double.infinity, child: ,),
+      ],
+    );
+  }
+}
